04a9b902996535eba87bf5580eed61f92e70cd5a
Tomasz Rup <tomasz.r@elcar-gps.com.pl>
2014-10-31 09:16:12 +0100

    refactoring and comments

--- main.js
+++ main.js
@@ -1,45 +1,32 @@
 (function (self) {
-    var previews;
+    var previews; // handler to preview area
+    var width, height; // thumbnails dimension
+    var dropboxId; // Hook to dropbox area
+    var fileSelectorInputId; // Hook to <input type='file'>
+    var fileSelectorId; // Hook to file selector element
 
-    self.init = function init() {
-        previews = document.getElementById('previews');
+    /**
+     * Initialize application
+     * @param options
+     */
+    self.init = function init(options) {
+        options = options || {};
+        previews = document.getElementById(options.previews_id || 'previews');
+        width = options.width || 150;
+        height = options.height || 150;
+        dropboxId = options.dropbox_id || 'dropbox';
+        fileSelectorInputId = options.file_selector_input_id || 'file_selector_input';
+        fileSelectorId = options.file_selector_id || 'file_selector';
 
-        var dropbox = document.getElementById("dropbox");
-        dropbox.addEventListener("dragenter", dragenter, false);
-        dropbox.addEventListener("dragover", dragover, false);
-        dropbox.addEventListener("drop", drop, false);
-
-        function dragenter(e) {
-            e.stopPropagation();
-            e.preventDefault();
-        }
-
-        function dragover(e) {
-            e.stopPropagation();
-            e.preventDefault();
-        }
-
-        function drop(e) {
-            e.stopPropagation();
-            e.preventDefault();
-
-            var dt = e.dataTransfer;
-            var files = dt.files;
-
-            handleFiles(files);
-        }
-
-        var a = document.getElementById('fileSelect');
-        a.addEventListener('click', function (e) {
-            var inp = document.getElementById('fileElem');
-            if (inp) {
-                inp.click();
-            }
-            e.preventDefault();
-        }, false);
+        enableDropbox();
+        enableFileSelector();
     };
 
-    self.handleFiles = function (files) {
+    /**
+     * Handle DnD and <input type='file'> events
+     * @param files
+     */
+    function handleFiles(files) {
         previews.innerHTML = '';
 
         for (var i = 0, numFiles = files.length; i < numFiles; ++i) {
@@ -51,18 +38,61 @@
 
             createThumbnail(file);
         }
-    };
+    }
 
+    /**
+     * Configure <input type='file'> element
+     */
+    function enableFileSelector() {
+        var fileSelector = document.getElementById(fileSelectorId);
+        var fileSelectorInput = document.getElementById(fileSelectorInputId);
+
+        fileSelector.addEventListener('click', function (e) {
+            fileSelectorInput.click();
+        }, false);
+
+        fileSelectorInput.addEventListener('change', function (e) {
+            handleFiles(e.target.files);
+        });
+    }
+
+    /**
+     * Configure dropbox (drag'n'drop drop area)
+     */
+    function enableDropbox() {
+        var dropbox = document.getElementById(dropboxId);
+
+        dropbox.addEventListener('dragenter', function (e) {
+            e.stopPropagation();
+            e.preventDefault();
+        }, false);
+        dropbox.addEventListener('dragover', function (e) {
+            e.stopPropagation();
+            e.preventDefault();
+        }, false);
+        dropbox.addEventListener('drop', function (e) {
+            e.stopPropagation();
+            e.preventDefault();
+
+            handleFiles(e.dataTransfer.files);
+        }, false);
+    }
+
+    /**
+     * Create thumbnail
+     * @param file
+     */
     function createThumbnail(file) {
         var reader = new FileReader();
         var img = document.createElement('img');
         var canvas = document.createElement('canvas');
-        canvas.width = canvas.height = 150;
+        canvas.width = width;
+        canvas.height = height;
         previews.appendChild(canvas);
 
         img.onload = (function (canvas) {
             return function (e) {
-                canvas.getContext('2d').drawImage(e.target, 0, 0, 150, 150);
+                canvas.getContext('2d').drawImage(e.target, 0, 0, width, height);
                 canvas.onclick = function () {
                     window.open(e.target.src);
                 };
